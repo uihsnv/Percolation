9ab3b350847f33706175af82e9ab36b999022a07
Vishnu V K <vishnugb@gmail.com>
2018-02-07 16:46:10 +0530

    split interactive program

--- percolation.py
+++ percolation.py
@@ -12,14 +12,8 @@ pool = np.array([True, False])
 
 size = 200
 p= 0.3
-im = plt.imshow(np.reshape(choices(pool, weights=[p,1-p], k=size**2), (size,size)))
 
-axp = plt.axes([0.25, 0.1, 0.65, 0.03])
-p = Slider(axp, 'p', 0.0, 1.0, valinit=0)
+lattice = choices(pool, weights=[p,1-p], k=size**2), (size,size)
 
-def update(val):
-    plt.imshow(np.reshape(choices(pool, weights=[p.val,1-(p.val)], k=size**2), (size,size)))
-
-p.on_changed(update)
-
+plt.imshow(np.reshape(lattice))
 plt.show()
