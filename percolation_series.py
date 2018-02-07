573064db40230c0703aa2e191bd8c1b34d91a442
Vishnu V K <vishnugb@gmail.com>
2018-02-08 01:55:21 +0530

    slide-show of percolation

---
+++ percolation_series.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Percolation
+"""
+
+from time import sleep
+from random import choices
+import matplotlib.pyplot as plt
+import numpy as np
+
+POOL = np.array([True, False])
+
+SIZE = 200
+PEAS = 40
+
+COUNT = 0
+P = np.linspace(0, 1, num=PEAS, endpoint=True)
+# (p' - p)/(1 - p)
+X = (P[1:] - P[:-1])/(1 - P[:-1])
+
+LATTICE = np.full((SIZE**2), False, dtype=bool)
+
+PEA = str(round(P[COUNT],2))
+plt.title("Percolation on a square lattice : p = "+PEA, fontsize='xx-large')
+plt.imshow(np.reshape(LATTICE, (SIZE, SIZE)))
+plt.pause(0.5)
+
+for fraction in np.nditer(X):
+    
+    COUNT = COUNT + 1
+    for i in np.nditer(np.where(np.logical_not(LATTICE))):
+        LATTICE[i] = choices(POOL, weights=[fraction, 1-fraction], k=1)[0]
+
+    PEA = str(round(P[COUNT],2))
+    plt.title("Percolation on a square lattice : p = "+PEA, fontsize='xx-large')
+    plt.imshow(np.reshape(LATTICE, (SIZE, SIZE)))
+    plt.draw()
+    plt.pause(0.5)
