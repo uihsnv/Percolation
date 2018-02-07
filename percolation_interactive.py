c1cec22a29781d26b6497510f2ad9a89126fed78
Vishnu V K <vishnugb@gmail.com>
2018-02-07 17:04:20 +0530

    syntax corrections

--- percolation_interactive.py
+++ percolation_interactive.py
@@ -8,17 +8,17 @@ from matplotlib.widgets import Slider
 import numpy as np
 from random import choices
 
-pool = np.array([True, False])
+POOL = np.array([True, False])
 
-size = 200
-p= 0.3
-im = plt.imshow(np.reshape(choices(pool, weights=[p,1-p], k=size**2), (size,size)))
+SIZE = 200
+P = 0.3
+im = plt.imshow(np.reshape(choices(POOL, weights=[P, 1-P], k=(SIZE**2)), (SIZE, SIZE)))
 
-axp = plt.axes([0.25, 0.1, 0.65, 0.03])
-p = Slider(axp, 'p', 0.0, 1.0, valinit=0)
+AXP = plt.axes([0.25, 0.1, 0.65, 0.03])
+P = Slider(AXP, 'p', 0.0, 1.0, valinit=0)
 
-def update(val):
-    plt.imshow(np.reshape(choices(pool, weights=[p.val,1-(p.val)], k=size**2), (size,size)))
+def update():
+    plt.imshow(np.reshape(choices(POOL, weights=[P.val, 1-(P.val)], k=(SIZE**2)), (SIZE, SIZE)))
 
 p.on_changed(update)
 
