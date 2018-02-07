3fff3b6588786e9442af5848bfd485c1383a85e1
Vishnu V K <vishnugb@gmail.com>
2018-02-07 16:55:10 +0530

    syntax corrections

--- percolation.py
+++ percolation.py
@@ -8,12 +8,12 @@ from matplotlib.widgets import Slider
 import numpy as np
 from random import choices
 
-pool = np.array([True, False])
+POOL = np.array([True, False])
 
-size = 200
-p= 0.3
+SIZE = 200
+P = 0.3
 
-lattice = choices(pool, weights=[p,1-p], k=size**2), (size,size)
+LATTICE = choices(POOL, weights=[P, 1-P], k=(SIZE**2))
 
-plt.imshow(np.reshape(lattice))
+plt.imshow(np.reshape(LATTICE, (SIZE, SIZE)))
 plt.show()
