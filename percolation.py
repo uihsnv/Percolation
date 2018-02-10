3f3eeb7a72614d33f9961a207c130456337abe26
Vishnu V. Krishnan <vishnugb@gmail.com>
2018-02-10 10:26:48 +0530

    syntax corrections

--- percolation.py
+++ percolation.py
@@ -11,7 +11,7 @@ POOL = np.array([True, False])
 SIZE = 100
 P = 0.41
 
-figManager = plt.get_current_fig_manager()
-figManager.window.showMaximized()
+FIGMANAGER = plt.get_current_fig_manager()
+FIGMANAGER.window.showMaximized()
 plt.imshow(np.reshape(np.random.choice(POOL, SIZE**2, True, [P, 1-P]), (SIZE, SIZE)))
 plt.show()
--- percolation_series.py
+++ percolation_series.py
@@ -19,9 +19,9 @@ X = (P[1:] - P[:-1])/(1 - P[:-1])
 LATTICE = np.full((SIZE**2), False, dtype=bool)
 #LATTICE = np.random.choice(POOL, SIZE**2, True, [0.5, 0.5])
 
-figManager = plt.get_current_fig_manager()
-figManager.window.showMaximized()
-PEA = str(round(P[COUNT],2))
+FIGMANAGER = plt.get_current_fig_manager()
+FIGMANAGER.window.showMaximized()
+PEA = str(round(P[COUNT], 2))
 plt.title("Percolation on a square lattice : p = "+PEA, fontsize='xx-large')
 plt.imshow(np.reshape(LATTICE, (SIZE, SIZE)))
 plt.pause(0.5)
@@ -32,7 +32,7 @@ for fraction in np.nditer(X):
     for i in np.nditer(np.where(np.logical_not(LATTICE))):
         LATTICE[i] = np.random.choice(POOL, SIZE**2, True, [fraction, 1-fraction])[0]
 
-    PEA = str(round(P[COUNT],2))
+    PEA = str(round(P[COUNT], 2))
     plt.title("Percolation on a square lattice : p = "+PEA, fontsize='xx-large')
     plt.imshow(np.reshape(LATTICE, (SIZE, SIZE)))
     plt.draw()
