8181618c56066dc0b26257c40c224f5bc52fb680
Vishnu V K <vishnugb@gmail.com>
2018-02-11 21:58:07 +0530

    trvial

--- percolation_clusters.py
+++ percolation_clusters.py
@@ -15,6 +15,7 @@ P = 0.999
 
 FIGMANAGER = plt.get_current_fig_manager()
 FIGMANAGER.window.showMaximized()
+
 PEA = str(round(P, 3))
 plt.title("Percolation on a square lattice : p = "+PEA, fontsize='xx-large')
 plt.imshow(np.reshape(np.random.choice(POOL, SIZE**2, True, [P, 1-P]), (SIZE, SIZE)))
